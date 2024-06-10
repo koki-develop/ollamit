03cb6ea6f31d204651b9281182195c45107f291c
koki-develop <kou.pg.0131@gmail.com>
2024-06-11 08:21:04 +0900

    Update root command to accept dry-run and model flags: added flagDryRun bool and flagModel string

--- cmd/root.go
+++ cmd/root.go
@@ -3,19 +3,39 @@ package cmd
 import (
 	"os"
 
+	"github.com/koki-develop/ollamit/internal/ollamit"
 	"github.com/spf13/cobra"
+)
+
+var (
+	flagDryRun bool   // --dry-run
+	flagModel  string // --model
 )
 
 var rootCmd = &cobra.Command{
 	Use: "ollamit",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg := &ollamit.Config{
+			DryRun: flagDryRun,
+			Model:  flagModel,
+		}
+
+		m := ollamit.New(cfg)
+		if err := m.Start(); err != nil {
+			return err
+		}
 		return nil
 	},
 }
 
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.Flags().BoolVar(&flagDryRun, "dry-run", false, "dry run")
+	rootCmd.Flags().StringVarP(&flagModel, "model", "m", "", "model name")
+	rootCmd.MarkFlagRequired("model")
 }
